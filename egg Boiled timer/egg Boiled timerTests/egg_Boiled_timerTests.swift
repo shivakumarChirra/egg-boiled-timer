b5d8b4bb1e2a9516ab666eda32060c3a83f0afad
shiva <chirrashivakumar5@gmail.com>
2025-05-12 19:26:49 +0530

    Add files via upload

---
+++ egg Boiled timer/egg Boiled timerTests/egg_Boiled_timerTests.swift
@@ -0,0 +1,17 @@
+//
+//  egg_Boiled_timerTests.swift
+//  egg Boiled timerTests
+//
+//  Created by shivakumar chirra on 08/05/25.
+//
+
+import Testing
+@testable import egg_Boiled_timer
+
+struct egg_Boiled_timerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
